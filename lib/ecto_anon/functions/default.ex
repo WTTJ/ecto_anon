7b51a555dd81e46cbac4a65efa768b8122311caf
Thomas Battiston <thomas.battiston@wttj.co>
2021-12-10 16:13:10 +0100

    feat: add EctoAnon.Functions.Default module

---
+++ lib/ecto_anon/functions/default.ex
@@ -0,0 +1,38 @@
+defmodule EctoAnon.Functions.Default do
+  @moduledoc """
+  Default anonymizing functions for basic Ecto types.
+  """
+
+  @default_integer 0
+  @default_float 0.0
+  @default_string "redacted"
+  @default_map %{}
+  @default_decimal Decimal.new("0.0")
+  @default_date ~D[1970-01-01]
+  @default_datetime ~U[1970-01-01 00:00:00Z]
+  @default_naive_datetime ~N[1970-01-01 00:00:00]
+  @default_time ~T[00:00:00.000]
+
+  @doc """
+  Apply default anonymizing function based on field type.
+  """
+  def run({type, value}), do: do_run(type, value)
+
+  defp do_run(:integer, _value), do: @default_integer
+  defp do_run(:float, _value), do: @default_float
+  defp do_run(:string, _value), do: @default_string
+  defp do_run(:map, _value), do: @default_map
+  defp do_run(:decimal, _value), do: @default_decimal
+  defp do_run(:date, _value), do: @default_date
+  defp do_run(:utc_datetime, _value), do: @default_datetime
+  defp do_run(:utc_datetime_usec, _value), do: @default_datetime
+  defp do_run(:naive_datetime, _value), do: @default_naive_datetime
+  defp do_run(:naive_datetime_usec, _value), do: @default_naive_datetime
+  defp do_run(:time, _value), do: @default_time
+  defp do_run(:time_usec, _value), do: @default_time
+
+  defp do_run(:boolean, value), do: value
+  defp do_run(:id, value), do: value
+  defp do_run(:binary_id, value), do: value
+  defp do_run(:binary, value), do: value
+end
---
+++ test/ecto_anon/functions/default_test.exs
@@ -0,0 +1,40 @@
+defmodule EctoAnon.Functions.DefaultTest do
+  use ExUnit.Case, async: true
+
+  @fixed_value_types [
+    integer: 0,
+    float: 0.0,
+    string: "redacted",
+    map: %{},
+    decimal: Decimal.new("0.0"),
+    date: ~D[1970-01-01],
+    utc_datetime: ~U[1970-01-01 00:00:00Z],
+    utc_datetime_usec: ~U[1970-01-01 00:00:00Z],
+    naive_datetime: ~N[1970-01-01 00:00:00],
+    naive_datetime_usec: ~N[1970-01-01 00:00:00],
+    time: ~T[00:00:00.000],
+    time_usec: ~T[00:00:00.000]
+  ]
+
+  @same_value_types [:boolean, :id, :binary_id, :binary]
+
+  describe "run/1" do
+    test "returns fixed value type when appropriate" do
+      matches =
+        Enum.map(@fixed_value_types, fn {type, value} ->
+          EctoAnon.Functions.Default.run({type, "value"}) == value
+        end)
+
+      assert Enum.all?(matches)
+    end
+
+    test "returns the same value when appropriate" do
+      matches =
+        Enum.map(@same_value_types, fn type ->
+          EctoAnon.Functions.Default.run({type, "value"}) == "value"
+        end)
+
+      assert Enum.all?(matches)
+    end
+  end
+end
