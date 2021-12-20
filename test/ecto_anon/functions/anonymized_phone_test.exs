316a19b3e6c4e970cb408998f0a5973a778ae777
Clément Quaresma <62239278+quaresc@users.noreply.github.com>
2021-12-20 10:07:43 +0100

    Update test/ecto_anon/functions/anonymized_phone_test.exs
    Co-authored-by: Thomas Battiston <thomas.battiston@wttj.co>

--- test/ecto_anon/functions/anonymized_phone_test.exs
+++ test/ecto_anon/functions/anonymized_phone_test.exs
@@ -2,7 +2,7 @@ defmodule EctoAnon.Functions.AnonymizedPhoneTest do
   use ExUnit.Case, async: true
   alias EctoAnon.Functions.AnonymizedPhone
 
-  describe "run/1" do
+  describe "run/3" do
     test "returns anonymized phone if type is string and value is not nil" do
       anonymized_phone = AnonymizedPhone.run(:string, "0601020304", [])
       assert anonymized_phone == "xx xx xx xx xx"
