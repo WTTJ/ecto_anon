91fb7ef6deb278d79bde1db9ac5643c6c04a1690
Clément Quaresma <clement.quaresma@wttj.co>
2022-05-18 11:36:16 +0200

    feat: added not_anonymized filter

--- test/ecto_anon/query_test.exs
+++ test/ecto_anon/query_test.exs
@@ -1,6 +1,8 @@
 defmodule EctoAnon.QueryTest do
   use ExUnit.Case, async: false
 
+  import EctoAnon.Query
+  import Ecto.Query
   alias EctoAnon.{Repo, User}
 
   setup do
@@ -29,6 +31,31 @@ defmodule EctoAnon.QueryTest do
       updated_user = Repo.get(User, user.id)
 
       assert updated_user.anonymized == true
+    end
+  end
+
+  describe "not_anonymized/1" do
+    test "not_anonymized returns a resources not anonymized" do
+      Repo.insert!(%User{email: "user@email.com", anonymized: false})
+
+      Repo.insert!(%User{
+        email: "redacted@email.com",
+        anonymized: true
+      })
+
+      query =
+        from(u in User, select: u)
+        |> not_anonymized()
+
+      emails =
+        Repo.all(query)
+        |> Enum.map(& &1.email)
+
+      assert emails
+             |> Enum.member?("user@email.com")
+
+      refute emails
+             |> Enum.member?("redacted@email.com")
     end
   end
 end
